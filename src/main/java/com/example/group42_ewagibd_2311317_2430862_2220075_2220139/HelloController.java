6d2dfaa6fb76eeda57d3513bf91e80ffa8b96815
sakiraahammed <sakira.ahammed@gmail.com>
2025-11-21 19:24:21 +0600

    B

---
+++ src/main/java/com/example/group42_ewagibd_2311317_2430862_2220075_2220139/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.group42_ewagibd_2311317_2430862_2220075_2220139;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.group42_ewagibd_2311317_2430862_2220075_2220139 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.group42_ewagibd_2311317_2430862_2220075_2220139 to javafx.fxml;
+    exports com.example.group42_ewagibd_2311317_2430862_2220075_2220139;
+}
