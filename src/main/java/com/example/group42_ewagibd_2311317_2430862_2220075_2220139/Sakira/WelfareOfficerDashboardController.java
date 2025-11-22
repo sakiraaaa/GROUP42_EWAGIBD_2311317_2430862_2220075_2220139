07f3b9477bfeb47d629fb8a3650d30d904e84137
sakiraahammed <sakira.ahammed@gmail.com>
2025-11-22 16:58:14 +0600

    2

---
+++ src/main/java/com/example/group42_ewagibd_2311317_2430862_2220075_2220139/Sakira/WelfareOfficerDashboardController.java
@@ -0,0 +1,46 @@
+package com.example.group42_ewagibd_2311317_2430862_2220075_2220139.Sakira;
+
+import javafx.event.ActionEvent;
+
+public class WelfareOfficerDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void SubmitWorkerAidRequestOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ConductEmergencyDrillOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void PostAwarenessOnWorkersRightAndBenefitsOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void UploadHealthAndSafetyInformationOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void NextOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ConductWorkerServiceOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ScheduleTrainingProgramOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void CreateIncidentReportOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void PostUpdatesOnCollaborationsAndPartnerships(ActionEvent actionEvent) {
+    }
+}
