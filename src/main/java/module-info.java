f9a6e30571ca5fa95f21774cbf55af7e15af9c27
sakiraahammed <sakira.ahammed@gmail.com>
2025-11-22 22:32:55 +0600

    3

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,6 @@ module com.example.group42_ewagibd_2311317_2430862_2220075_2220139 {
     requires javafx.fxml;
 
 
-    opens com.example.group42_ewagibd_2311317_2430862_2220075_2220139 to javafx.fxml;
+    opens com.example.group42_ewagibd_2311317_2430862_2220075_2220139.Sakira to javafx.fxml;
     exports com.example.group42_ewagibd_2311317_2430862_2220075_2220139;
 }
