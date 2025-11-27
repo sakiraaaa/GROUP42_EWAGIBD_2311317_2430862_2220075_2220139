2ffbc86a8928682be377e77c287e578a65ebb3ad
kmashrifa <fooyookomio@gmail.com>
2025-11-27 23:05:18 +0600

    2nd part commit

--- src/main/java/com/example/group42_ewagibd_2311317_2430862_2220075_2220139/Mashrifa/feedbackFormController.java
+++ src/main/java/com/example/group42_ewagibd_2311317_2430862_2220075_2220139/Mashrifa/feedbackFormController.java
@@ -1,7 +1,28 @@
 package com.example.group42_ewagibd_2311317_2430862_2220075_2220139.Mashrifa;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class feedbackFormController
 {
     @javafx.fxml.FXML
+    private TextArea feedbackMassageArea;
+    @javafx.fxml.FXML
+    private Label massageLabel;
+    @javafx.fxml.FXML
+    private Label sebdFeedbackLabel;
+    @javafx.fxml.FXML
+    private Label subjectLabel;
+    @javafx.fxml.FXML
+    private TextField feedbackSubjectField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void sendFeedbackOnAction(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/example/group42_ewagibd_2311317_2430862_2220075_2220139/Mashrifa/viewClaimStatusController.java
+++ src/main/java/com/example/group42_ewagibd_2311317_2430862_2220075_2220139/Mashrifa/viewClaimStatusController.java
@@ -1,7 +1,38 @@
 package com.example.group42_ewagibd_2311317_2430862_2220075_2220139.Mashrifa;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class viewClaimStatusController
 {
     @javafx.fxml.FXML
+    private TextField searchClaimsField;
+    @javafx.fxml.FXML
+    private TableColumn statusColumn;
+    @javafx.fxml.FXML
+    private TableColumn amountColumn;
+    @javafx.fxml.FXML
+    private TableView tableView;
+    @javafx.fxml.FXML
+    private TableColumn dateColumn;
+    @javafx.fxml.FXML
+    private Label claimStatusLabel;
+    @javafx.fxml.FXML
+    private TableColumn typeColumn;
+    @javafx.fxml.FXML
+    private TableColumn claimIdColumn;
+    @javafx.fxml.FXML
+    private ComboBox statusComboBox;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void searchClaimsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clearClaimsOnAction(ActionEvent actionEvent) {
+    }
+}
