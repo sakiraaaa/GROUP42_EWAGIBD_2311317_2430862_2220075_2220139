fd57c99e2b886ebb126486079d9ed516b15b74ca
kmashrifa <fooyookomio@gmail.com>
2025-12-02 00:11:34 +0600

    4th part commit

--- src/main/java/com/example/group42_ewagibd_2311317_2430862_2220075_2220139/Mashrifa/EmployeeDashboardController.java
+++ src/main/java/com/example/group42_ewagibd_2311317_2430862_2220075_2220139/Mashrifa/EmployeeDashboardController.java
@@ -1,5 +1,8 @@
 package com.example.group42_ewagibd_2311317_2430862_2220075_2220139.Mashrifa;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class EmployeeDashboardController
 {
     @javafx.fxml.FXML
