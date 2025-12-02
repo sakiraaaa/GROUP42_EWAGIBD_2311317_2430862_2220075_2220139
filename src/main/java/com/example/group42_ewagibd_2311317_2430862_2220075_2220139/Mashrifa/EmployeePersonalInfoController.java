c3833c48740988b94d8ecc414b00c4294538488b
kmashrifa <fooyookomio@gmail.com>
2025-12-03 00:29:26 +0600

    fxml

--- src/main/java/com/example/group42_ewagibd_2311317_2430862_2220075_2220139/Mashrifa/EmployeePersonalInfoController.java
+++ src/main/java/com/example/group42_ewagibd_2311317_2430862_2220075_2220139/Mashrifa/EmployeePersonalInfoController.java
@@ -1,6 +1,6 @@
 package com.example.group42_ewagibd_2311317_2430862_2220075_2220139.Mashrifa;
 
-public class TrainingDetailsController
+public class EmployeePersonalInfoController
 {
     @javafx.fxml.FXML
     public void initialize() {
