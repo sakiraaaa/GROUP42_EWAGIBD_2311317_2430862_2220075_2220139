473cbaa5605c0fac7728670bf5c1bda63e5d35b0
sakiraahammed <sakira.ahammed@gmail.com>
2025-12-08 09:59:14 +0600

    19

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,16 @@
 module com.example.group42_ewagibd_2311317_2430862_2220075_2220139 {
+
     requires javafx.controls;
     requires javafx.fxml;
-
     requires javafx.graphics;
 
 
+    opens com.example.group42_ewagibd_2311317_2430862_2220075_2220139 to javafx.fxml;
     opens com.example.group42_ewagibd_2311317_2430862_2220075_2220139.Sakira to javafx.fxml;
     opens com.example.group42_ewagibd_2311317_2430862_2220075_2220139.Mashrifa to javafx.fxml;
+
+
     exports com.example.group42_ewagibd_2311317_2430862_2220075_2220139;
+    exports com.example.group42_ewagibd_2311317_2430862_2220075_2220139.Sakira;
+    exports com.example.group42_ewagibd_2311317_2430862_2220075_2220139.Mashrifa;
 }
